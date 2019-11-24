3b380171f92c055d04f074554ce5f07cc54b875a
Andrew Davis <atd285@gmail.com>
2019-11-24 17:06:51 -0500

    play/pause controls along with fullscreen

--- app.js
+++ app.js
@@ -1,21 +1,53 @@
 const video = document.querySelector('.video-container video');
-
-//play button
+const videoContainer = document.querySelector('.video-container');
+// play button
 const playButton = document.querySelector(
 	'.video-container .controls button.play',
 );
-
-//rewind
+// rewind
 const rewind = document.querySelector(
 	'.video-container .controls button.rewind',
 );
-
-//fast forward
+// fast forward
 const fastForward = document.querySelector(
 	'.video-container .controls button.fast-forward',
 );
+// volume
+const toggleVolume = document.querySelector(
+	'.video-container .controls button.volume',
+);
 
-playButton.addEventListener('click', function(e) {
-	console.log('clicked');
-	video.play();
+const fullScreen = document.querySelector(
+	'.video-container .controls button.full-screen',
+);
+
+// eslint-disable-next-line
+playButton.addEventListener('click', () => {
+	if (video.paused) {
+		// we are in a paused state, on next click the video will play
+		video.play();
+	} else {
+		video.pause();
+	}
+});
+
+fastForward.addEventListener('click', () => {
+	video.currentTime += 10;
+});
+
+rewind.addEventListener('click', () => {
+	video.currentTime -= 10;
+});
+
+// toggle between mute states
+toggleVolume.addEventListener('click', () => {
+	video.muted = !video.muted;
+});
+
+fullScreen.addEventListener('click', () => {
+	if (!document.fullscreenElement) {
+		videoContainer.requestFullscreen();
+	} else {
+		document.exitFullscreen();
+	}
 });
